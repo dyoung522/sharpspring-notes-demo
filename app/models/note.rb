3c074af13e821dc23b2e3c0b99af6087b53079e4
Donovan Young <dyoung522@gmail.com>
2020-02-13 23:56:21 -0500

    Note title defaults to first 30 chars of body when not provided

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,4 +5,10 @@ class Note < ApplicationRecord
 
   validates :title, presence: true, length: {maximum: 30}
   validates :body, length: {maximum: 1000}
+
+  before_validation :check_title
+
+  def check_title
+    self.title = body.truncate(30) if title.blank? && body
+  end
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,8 +3,20 @@
 require "rails_helper"
 
 RSpec.describe Note, type: :model do
+  let(:user) { create(:user) }
+
   it { should belong_to(:user) }
   it { should validate_presence_of(:title) }
   it { should validate_length_of(:title) }
   it { should validate_length_of(:body) }
+
+  it "uses the first 30 characters of the body for the title" do
+    body = Faker::Lorem.paragraph(sentence_count: 10)
+    note = Note.create(body: body, user: user)
+    expect(note.title).to eq body.truncate(30)
+  end
+
+  it "is not valid when both title and body are empty" do
+    expect(Note.new(user: user)).not_to be_valid
+  end
 end
