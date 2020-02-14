b32b64cc4ae51dc8223454870dceded6e2dd7578
Donovan Young <dyoung522@gmail.com>
2020-02-14 00:48:33 -0500

    Flash alerts remain for 30 seconds
    Notices still fade after 10

--- app/javascript/notes/index.js
+++ app/javascript/notes/index.js
@@ -1,5 +1,8 @@
 $(document).on("turbolinks:load", () => {
-  $(".flash")
+  $(".flash-notice")
     .delay(10000)
+    .fadeOut(1000);
+  $(".flash-alert")
+    .delay(30000)
     .fadeOut(1000);
 });
